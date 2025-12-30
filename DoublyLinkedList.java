e78acd29a34b52957165049fd0a33bd4b1c7c4a7
Polwaththa Gallage Maheli Navinduni <mahelinavinduni@gmail.com>
2025-12-30 22:59:48 +0530

    Add files via upload

---
+++ DoublyLinkedList.java
@@ -0,0 +1,52 @@
+package dsa;
+
+class DoublyLinkedList {
+
+    class Node {
+        int data;
+        Node prev;
+        Node next;
+
+        Node(int d) {
+            data = d;
+            prev = null;
+            next = null;
+        }
+    }
+
+    Node head = null;
+
+    void insert(int data) {
+        Node newNode = new Node(data);
+
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+
+        Node temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+
+        temp.next = newNode;
+        newNode.prev = temp;
+    }
+
+    void display() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " <-> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        DoublyLinkedList list = new DoublyLinkedList();
+        list.insert(5);
+        list.insert(15);
+        list.insert(25);
+        list.display();
+    }
+}
